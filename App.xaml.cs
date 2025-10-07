5d1269507d2c5a16860b4715c7829701bd04cd14
Unillion <114824496+Unillion@users.noreply.github.com>
2025-10-07 20:52:18 +0700

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GamesShop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
