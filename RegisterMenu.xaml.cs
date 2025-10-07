d6ed16765528db9deaafa26068148367429643bb
Unillion <114824496+Unillion@users.noreply.github.com>
2025-10-08 02:10:27 +0700

    v1

---
+++ RegisterMenu.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GamesShop
+{
+    public partial class RegisterMenu : Window
+    {
+
+        bool canRegister = true;
+
+        public RegisterMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!canRegister) return;
+            UserManager.RegisterUser(UsernameTextBox.Text, EmailTextBox.Text, ConfirmPasswordBox.Password);
+
+            UsernameTextBox.Text = string.Empty;
+            EmailTextBox.Text = string.Empty;
+            PasswordBox.Password = string.Empty;
+
+            MessageBox.Show("Регистрация прошла успешно. Войдите в систему");
+            LoginMenu loginMenu = new LoginMenu();
+            loginMenu.Show();
+
+            this.Close();
+        }
+
+        private void GoToLogin_Click(object sender, RoutedEventArgs e)
+        {
+            LoginMenu loginWindow = new LoginMenu();
+            loginWindow.Show();
+
+            this.Close();
+        }
+
+        private void ChangedConfirmPassword(object sender, RoutedEventArgs e)
+        {
+            string password = PasswordBox.Password;
+            string confPassword = ConfirmPasswordBox.Password;
+
+            if (password != confPassword)
+            {
+                PasswordError.Visibility = Visibility.Visible;
+            }
+            else PasswordError.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ user/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesShop
+{
+    public class User
+    { 
+
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string name, string email, string password)
+        {
+            UserName = name;
+            Email = email;
+            Password = password;
+        }
+
+    }
+}
